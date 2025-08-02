a2a97d0b42e768b8ee52f68c13eccd4139e327f4
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2025-08-02 18:46:10 +0530

    update sidebar page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -30,9 +30,21 @@ function Sidebar({selectedUser, setSelectedUser}) {
 
 
       <div className='flex flex-col'>
-        {userDummyData.map((user, idx) => (
-          <div>
+        {userDummyData.map((user, index) => (
+          <div className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm`}>
             <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full' />
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {
+                index < 3 
+                ? <span className='text-green-400 text-xs'>online</span>
+                : <span className='text-neutral-400 text-xs'>offline</span>
+              }
+            </div>
+
+            {
+              index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>
+            }
           </div>
         )
       )}
